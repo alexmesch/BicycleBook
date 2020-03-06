8aeb95c8f213f2ccf4bb377a9237c4bed5b20b03
Alex Mesch <mesheryakov.alexander@mail.ru>
2020-03-06 10:34:32 +0300

    A little bit of animation added to SignIn screen

--- app/src/main/java/com/msch/bicyclebook/SignIn.java
+++ app/src/main/java/com/msch/bicyclebook/SignIn.java
@@ -3,15 +3,24 @@ package com.msch.bicyclebook;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SignIn extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+
+        View anim_elements = findViewById(R.id.signIn_container);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha_transition);
+        anim_elements.startAnimation(anim);
+
     }
 
-    public void returnToMaps(View v){
+    public void returnToMaps(View v) {
         finish();
     }
 }
